164e30c8e3824d37217baf1655eed54900c5a497
ralcaraz <ralcarazerhs@gmail.com>
2022-06-14 14:54:19 -0700

    Fixed incorrect property name and changed setter to inits

--- Ralcaraz.Microservices.Common/Models/AzureRegistryPush.cs
+++ Ralcaraz.Microservices.Common/Models/AzureRegistryPush.cs
@@ -2,27 +2,27 @@ namespace Ralcaraz.Microservices.Common.Models;
 
 public record AzureRegistryPush
 {
-    public string Id { get; set; }
-    public DateTime TimeStamp { get; set; }
-    public string Action { get; set; }
-    public RegistryTarget? Target { get; set; }
-    public RegistryRequest? Request { get; set; }
+    public string Id { get; init; }
+    public DateTime TimeStamp { get; init; }
+    public string Action { get; init; }
+    public RegistryTarget? Target { get; init; }
+    public RegistryRequest? Request { get; init; }
 }
 
 public record RegistryTarget
 {
-    public string MediaType { get; set; }
-    public int Size { get; set; }
-    public string Digest { get; set; }
-    public int Length { get; set; }
-    public string? Registry { get; set; }
-    public string? Tag { get; set; }
+    public string MediaType { get; init; }
+    public int Size { get; init; }
+    public string Digest { get; init; }
+    public int Length { get; init; }
+    public string? Repository { get; init; }
+    public string? Tag { get; init; }
 }
 
 public record RegistryRequest
 {
-    public string Id { get; set; }
-    public string? Host { get; set; }
-    public string? Method { get; set; }
-    public string? UserAgent { get; set; }
+    public string Id { get; init; }
+    public string? Host { get; init; }
+    public string? Method { get; init; }
+    public string? UserAgent { get; init; }
 }
